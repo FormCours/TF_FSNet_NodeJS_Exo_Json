a5282494ce9815f14e8048c4e3a1ee88da7075e0
Aurélien <formation.net@outlook.be>
2020-10-02 17:09:14 +0200

    Correction ;)

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const {extractJson} = require('./modules/json-helper');
 
 
+const getPersonnages = (data) => {
+    if(data.membre !== undefined) {
+        let personnages = [];
+        for(const membre of data.membre) {
+            personnages = [...personnages, ...getPersonnages(membre)]
+        }
+        return personnages
+    }
+    else {
+        return [data];
+    }
+}
+
+
 const app = async () => {
     const data = await extractJson('./data/personnage.json');
-    console.log(data);
+
+    const personnages = getPersonnages(data);
+    
+    console.log(personnages);
 }
 app();
